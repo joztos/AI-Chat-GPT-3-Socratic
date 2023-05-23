d4b010f38a946b88a1742c661e43ee1d899324d6
Your Name <you@example.com>
2023-05-23 17:43:14 -0600

    back to basics

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,10 @@
 import type { FC, ReactNode } from 'react'
+import Head from 'next/head.js'
+import Nav, { NavProps } from './Nav'
+import Logo from './Steamship'
 import { ComponentType } from 'react'
 
-export interface LayoutProps {
-  path: string;
+export interface LayoutProps extends NavProps {
   children?: ReactNode
   title?: string
   description?: string
@@ -12,30 +14,42 @@ const Layout: FC<LayoutProps> = ({
   title,
   description,
   path,
+  deployButton,
   children,
 }) => {
   return (
     <div className="mx-auto h-screen flex flex-col">
+      <Head>
+        {title && <title>{`${title} - Steamship + Vercel Examples`}</title>}
+        {description && <meta name="description" content={description} />}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Nav path={path} deployButton={deployButton} />
+
       <div className="px-8 bg-accents-0">{children}</div>
 
       <footer className="py-10 w-full mt-auto border-t flex items-center justify-center bg-accents-1 z-20">
-        <span className="text-primary">Created by Samasat Group for <strong>EMDI</strong> school 
-        </span>
+        <span className="text-primary">Created by</span>
         <a
-          href="https://www.emdischool.com/"
-          aria-label="EMDISchool.com Link"
+          href="https://steamship.com"
+          aria-label="Steamship.com Link"
           target="_blank"
           rel="noreferrer"
           className="text-black "
         >
+          <Logo
+            className="inline-block h-6 ml-3 text-primary"
+          /> Samasat
         </a>. Powered
         <a
           href="https://vercel.com/templates/next.js/ai-gpt3-chatbot"
           target="_blank"
           rel="noreferrer"
           className="text-black ml-1 mr-1"
-        > via <strong>Navi AI</strong>.
+        > by gpt3
         </a> .
+
       </footer>
     </div>
   )
@@ -43,7 +57,14 @@ const Layout: FC<LayoutProps> = ({
 
 export default Layout
 
+
 const Noop: FC<{ children?: ReactNode }> = ({ children }) => <>{children}</>
+
+export interface LayoutProps extends NavProps {
+    children?: ReactNode;
+    title?: string;
+    description?: string;
+}
 
 export function getLayout<LP extends {}>(
   Component: ComponentType<any>

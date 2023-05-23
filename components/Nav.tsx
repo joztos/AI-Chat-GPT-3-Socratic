7de6fc83bc3a703f0585515dd9cfd9017b4434d4
Your Name <you@example.com>
2023-05-23 17:37:23 -0600

    backup header

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,30 +1,28 @@
 import Link from '@vercel/examples-ui/link'
+import Button from '@vercel/examples-ui/button'
+import DeployButton, { DeployButtonProps } from '@vercel/examples-ui/deploy-button'
 import { Layout, Text, Page } from '@vercel/examples-ui'
-import Image from 'next/image'
+import Logo from './Steamship'
 
 const REPO_URL = 'https://github.com/steamship-core/vercel-examples/tree/main'
 
 export interface NavProps {
   path: string
+  deployButton?: Partial<DeployButtonProps>
 }
 
-export default function Nav({ path }: NavProps) {
-  const repositoryUrl = `${REPO_URL}/${path}`
+export default function Nav({ path, deployButton }: NavProps) {
+  const displayPath = ['Vercel Examples']
+    .concat(path?.split('/').filter(Boolean) || [])
+    .join(' / ')
+  const repositoryUrl = deployButton?.repositoryUrl || `${REPO_URL}/${path}`
 
   return (
     <nav className="border-b border-gray-200 py-5 relative z-20 bg-background shadow-[0_0_15px_0_rgb(0,0,0,0.1)]">
       <div className="flex items-center lg:px-6 px-8 mx-auto max-w-7xl px-14">
-        <div className="flex flex-row items-center space-x-3">
-          <Link href="https://www.emdischool.com/">
-            <div className="flex items-center">
-              <Image
-                alt="EMDI School Logo"
-                src="https://i.im.ge/2023/04/11/IBXuyY.EMDILOGO.png"
-                className="object-contain"
-                width={120}
-                height={48}
-              />
-            </div>
+        <div className="flex flex-row items-center">
+          <Link href="https://www.steamship.com/">
+            <Logo className={`w-8 h-8 text-black`} ></Logo>
           </Link>
           <ul className="flex items-center content-center">
             <li className="ml-2 text-gray-200">
@@ -42,17 +40,39 @@ export default function Nav({ path }: NavProps) {
                 <path d="M16.88 3.549L7.12 20.451"></path>
               </svg>
             </li>
+            <li className="font-medium" style={{ letterSpacing: '.01px' }}>
+              <Link
+                href={repositoryUrl}
+                className="text-accents-6 no-underline transition-colors duration-200 hover:text-accents-8 cursor-pointer"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {displayPath}
+              </Link>
+            </li>
           </ul>
         </div>
         <div className="flex-1 justify-end hidden md:flex">
           <nav className="flex-row inline-flex items-center">
-            <Image
-              alt="Vercel Icon"
-              src="/vercelLogo.png"
-              className="sm:w-8 sm:h-[27px] w-8 h-[28px]"
-              width={32}
-              height={28}
-            />
+            <span className="ml-2 h-full flex items-center cursor-not-allowed text-accents-5">
+              <Button
+                variant="ghost"
+                Component="a"
+                href="https://github.com/steamship-core/vercel-examples"
+                target="_blank"
+                rel="noreferrer"
+              >
+                More Examples →
+              </Button>
+            </span>
+            <span className="ml-2 h-full flex items-center cursor-not-allowed text-accents-5">
+              <DeployButton
+                {...deployButton}
+                repositoryUrl={repositoryUrl}
+                projectName={deployButton?.projectName || path}
+                repositoryName={deployButton?.repositoryName || path}
+              />
+            </span>
           </nav>
         </div>
       </div>

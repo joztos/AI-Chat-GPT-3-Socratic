27435d6513f66d4e4497ef0455b40c510a1b72fd
Your Name <you@example.com>
2023-05-23 16:57:43 -0600

    redi

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,12 @@
+import type { FC, ReactNode } from 'react'
+import { ComponentType } from 'react'
+
+export interface LayoutProps {
+  path: string;
+  children?: ReactNode
+}
+
 const Layout: FC<LayoutProps> = ({
-  title,
-  description,
   path,
   children,
 }) => {
@@ -17,7 +23,6 @@ const Layout: FC<LayoutProps> = ({
           rel="noreferrer"
           className="text-black "
         >
-          
         </a>. Powered
         <a
           href="https://vercel.com/templates/next.js/ai-gpt3-chatbot"
@@ -34,13 +39,6 @@ const Layout: FC<LayoutProps> = ({
 export default Layout
 
 const Noop: FC<{ children?: ReactNode }> = ({ children }) => <>{children}</>
-
-export interface LayoutProps {
-  path: string;
-  children?: ReactNode;
-  title?: string;
-  description?: string;
-}
 
 export function getLayout<LP extends {}>(
   Component: ComponentType<any>

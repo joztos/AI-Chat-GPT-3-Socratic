b2331f5bb7726e940b67a9290a7a9e312636edb9
Your Name <you@example.com>
2023-05-23 17:49:14 -0600

    bai

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,6 @@
 import type { FC, ReactNode } from 'react'
 import Head from 'next/head.js'
 import Nav, { NavProps } from './Nav'
-import Logo from './Steamship'
 import { ComponentType } from 'react'
 
 export interface LayoutProps extends NavProps {
@@ -30,33 +29,29 @@ const Layout: FC<LayoutProps> = ({
       <div className="px-8 bg-accents-0">{children}</div>
 
       <footer className="py-10 w-full mt-auto border-t flex items-center justify-center bg-accents-1 z-20">
-        <span className="text-primary">Created by</span>
+        <span className="text-primary">Created by Samasat Group for </span>
         <a
-          href="https://steamship.com"
-          aria-label="Steamship.com Link"
+          href="https://www.emdischool.com/"
+          aria-label="EMDISchool.com Link"
           target="_blank"
           rel="noreferrer"
           className="text-black "
         >
-          <Logo
-            className="inline-block h-6 ml-3 text-primary"
-          /> Samasat
+          <strong>EMDI school</strong>
         </a>. Powered
         <a
           href="https://vercel.com/templates/next.js/ai-gpt3-chatbot"
           target="_blank"
           rel="noreferrer"
           className="text-black ml-1 mr-1"
-        > by gpt3
+        > via <strong>Navi AI</strong>.
         </a> .
-
       </footer>
     </div>
   )
 }
 
 export default Layout
-
 
 const Noop: FC<{ children?: ReactNode }> = ({ children }) => <>{children}</>
 

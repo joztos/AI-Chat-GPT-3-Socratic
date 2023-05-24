2f6f5621fb0169c14941a287be70d3f45de2e4fc
Your Name <you@example.com>
2023-05-23 18:49:06 -0600

    yano?a

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,8 +7,7 @@ export interface NavProps {
 }
 
 export default function Nav({ path }: NavProps) {
-  const displayPath = path.split('/').filter(Boolean).join(' / ');
-  const repositoryUrl = `https://samasat.com/${path}`;
+  const repositoryUrl = `https://samasat.com`;
 
   return (
     <nav className="border-b border-gray-200 py-5 relative z-20 bg-background shadow-[0_0_15px_0_rgb(0,0,0,0.1)]">
@@ -22,33 +21,6 @@ export default function Nav({ path }: NavProps) {
               height={32}
             />
           </Link>
-          <ul className="flex items-center content-center">
-            <li className="ml-2 text-gray-200">
-              <svg
-                viewBox="0 0 24 24"
-                width="32"
-                height="32"
-                stroke="currentColor"
-                strokeWidth="1"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                fill="none"
-                shapeRendering="geometricPrecision"
-              >
-                <path d="M16.88 3.549L7.12 20.451"></path>
-              </svg>
-            </li>
-            <li className="font-medium" style={{ letterSpacing: '.01px' }}>
-              <Link
-                href={repositoryUrl}
-                className="text-accents-6 no-underline transition-colors duration-200 hover:text-accents-8 cursor-pointer"
-                target="_blank"
-                rel="noreferrer"
-              >
-                {displayPath}
-              </Link>
-            </li>
-          </ul>
         </div>
         <div className="flex items-center">
           <Image

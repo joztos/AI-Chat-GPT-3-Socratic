1d903d115a7b9757fd038af48afbcbc55500c374
Your Name <you@example.com>
2023-06-07 15:33:55 -0600

    update ts

--- types.d.ts
+++ types.d.ts
@@ -1,1 +1,8 @@
-declare module 'annyang';
+declare module 'annyang' {
+    export function addCommands(commands: any): void;
+    export function start(): void;
+    export function abort(): void;
+    export function addCallback(type: string, callback: Function): void;
+    // Add other functions from annyang you want to use
+  }
+  
